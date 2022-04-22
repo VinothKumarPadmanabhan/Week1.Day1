2166f6aea431d0eccf225000411adc3efadf004c
VinothKumarPadmanabhan <104205222+VinothKumarPadmanabhan@users.noreply.github.com>
2022-04-22 17:28:38 +0530

    Add files via upload

---
+++ LearnDatatypes.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+int Noofseats=5;
+boolean manualgear=true;
+char model= 'z';
+double petrolcapacity=22.56;
+short musicsystem=21000;
+long carmodelno=3455754677L;
+byte speedometer=120;
+float mileage=16.66f;
+
+
+
+
+
+
+
+}
